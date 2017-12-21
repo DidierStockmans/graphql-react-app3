db4da38d09f989d5c07b1f4052cf328d6c377fb1
Didier Stockmans <stockmans.didier@gmail.com>
2017-12-21 21:29:41 +0100

    Current user query. Passing cookies with requests.

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header'; 
 
-const App = (props) => {console.log(1);
+const App = (props) => {
     return (
         <div>
             <Header />
--- client/components/Header.js
+++ client/components/Header.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import {graphql} from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
 
+    renderButtons() {
+        const { loading, user } = this.props.data;
+
+        if (loading) return <div />;
+
+        if (user) {
+            return <div>Logout</div>;
+        } else {
+            return (
+                <div>You're not signed in.</div>
+            );
+        }
+    }
+
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="wrapper">
+                    {this.renderButtons()}
+                </div>
+            </nav>
         )
     }
 }
 
-export default Header;
+export default graphql(query)(Header);
